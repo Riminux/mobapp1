9544a11ea56c49b697255cb1bdd4e229238a0756
Riminux <raimund50.lol@gmail.com>
2024-10-01 14:20:50 +0300

    Change2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Empty Flutter App',
+      title: 'MIPT Praktinis 1',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
@@ -20,17 +20,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _displayText = 'Hi';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Empty Flutter App'),
-      ),
-      body: const Center(
-        child: Text('Hello, World!'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _displayText,
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            // Button widget
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _displayText = 'Bye.';
+                });
+              },
+              child: const Text('Press Me :)'),
+            ),
+          ],
+        ),
       ),
     );
   }
